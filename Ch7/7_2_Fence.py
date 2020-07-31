20c9bb5eff94e9f9dba38a95cd270d82d34379d4
jinukoo <jinukoo@naver.com>
2020-07-31 16:12:12 +0900

    python code with book algorithm but time over

--- Ch7/7_2_Fence.py
+++ Ch7/7_2_Fence.py
@@ -1,7 +1,44 @@
 # Q : from fence made out of N woods, find the size of the largest reactange
 
-# Idea : Brute Force
-# find all possible reactangles and figure out the max size
+# Idea : Divide and Conquer
+# divide the fence in half
+# the largest should be either in the left, in the right, or sideded in the middle
+
+
+def solve(left, right):
+    # base case : left == right
+    if left == right :
+        return heights[left]
+    
+    half = (left + right) // 2
+
+    # check largest in the left and right
+    res = max(solve(left, half), solve(half + 1, right))
+
+    # check largest in the middle
+    low = half
+    high = half + 1
+    
+    # check smallest middle position rectangle (size 2)
+    height = min(heights[low], heights[high])
+    res = max(res, height * 2)
+
+    # iterate until cannot expand to left and right
+    while left < low or high < right :
+        # expand to more larger height
+        # expand to right
+        if high < right and (low == left or heights[low - 1] < heights[high + 1]):
+            high += 1
+            height = min(height, heights[high])
+        # expand to left
+        else :
+            low -= 1
+            height = min(height, heights[low])
+        
+        # compare area
+        res = max(res, height * (high - low + 1))
+
+    return res
 
 
 # input test cases
@@ -10,35 +47,6 @@ for i in range(c):
     # input number of woods
     n = int(input())
     heights = list(map(int, input().split()))
-    
-    max = 0
-    for j in range(n):
-        length = 1
-        # find left side length
-        satisfied = True
-        left = j - 1
-        while(satisfied):
-            if left >= 0 and heights[left] >= heights[j]:
-                length += 1
-                left -= 1
-            else:
-                satisfied = False
+    print(solve(0, n - 1))
 
-        # find right side length
-        satisfied = True
-        right = j + 1
-        while(satisfied):
-            if right < n and heights[right] >= heights[j]:
-                length += 1
-                right += 1
-            else:
-                satisfied = False
-        
-        # calculate area
-        area = length * heights[j]
-
-        # save the largest area
-        if area > max :
-            max = area
-    
-    print(max)
+# O(n) = nlogn algorithm
